d6911d332761204d2cdef658317b70700b6ff2d6
LucaZanni <info@pzeta.it>
2024-07-05 19:36:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# K8s YAML Generator GitHub Action
+
+This GitHub Action generates Kubernetes YAML files from configuration and environment variables. It leverages inputs from the user to create ConfigMap, Service, Deployment, and HorizontalPodAutoscaler YAML files.
+
+## Usage
+
+To use this action in your GitHub workflow, add the following steps to your `.github/workflows/*.yml` file:
+
+```yaml
+name: Generate K8s YAML
+
+on: [push]
+
+jobs:
+  generate-yaml:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+      - name: Generate K8s YAML
+        uses: ./
+        with:
+          instance: 'my-instance'
+          namespace: 'my-namespace'
+          serviceType: 'ClusterIP'
+          targetPort: 8080
+          replicas: 3
+          registry: 'my-registry'
+          owner: 'my-owner'
+          minReplicas: 1
+          maxReplicas: 5
+          targetCPUUtilizationPercentage: 75
+          verbose: 'true'
+          path: './config'
+```
+
+## Inputs
+
+| Input                            | Description                                                   | Required | Default            |
+|----------------------------------|---------------------------------------------------------------|----------|--------------------|
+| `instance`                       | Instance of the application                                   | No       | `''`               |
+| `namespace`                      | Namespace for Kubernetes                                      | No       | `default`          |
+| `serviceType`                    | Type of Kubernetes Service                                    | No       | `ClusterIP`        |
+| `targetPort`                     | Target port for the service                                   | No       | `8080`             |
+| `replicas`                       | Number of replicas for the deployment                         | No       | `1`                |
+| `registry`                       | Docker registry for the image                                 | Yes      |                    |
+| `owner`                          | Owner of the Docker image                                     | Yes      |                    |
+| `minReplicas`                    | Minimum replicas for the HorizontalPodAutoscaler              | No       | `1`                |
+| `maxReplicas`                    | Maximum replicas for the HorizontalPodAutoscaler              | No       | `3`                |
+| `targetCPUUtilizationPercentage` | Target CPU Utilization Percentage for the HorizontalPodAutoscaler | No       | `80`               |
+| `verbose`                        | Enable verbose logging                                        | No       | `false`            |
+| `path`                           | Directory path for package.json, build-info.json, and .env files | No       | `.`                |
+
+## Outputs
+
+This action does not produce any outputs.
+
+## Example
+
+Below is an example of a GitHub workflow that uses this action:
+
+```yaml
+name: Generate K8s YAML
+
+on: [push]
+
+jobs:
+  generate-yaml:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+      - name: Generate K8s YAML
+        uses: ./
+        with:
+          instance: 'example-instance'
+          namespace: 'example-namespace'
+          serviceType: 'ClusterIP'
+          targetPort: 8080
+          replicas: 2
+          registry: 'dockerhub.io'
+          owner: 'example-owner'
+          minReplicas: 1
+          maxReplicas: 5
+          targetCPUUtilizationPercentage: 75
+          verbose: 'true'
+          path: './config'
+```
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request with your changes.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ index.js
@@ -0,0 +1,187 @@
+const core = require('@actions/core');
+const fs = require("fs");
+const yaml = require("js-yaml");
+const dotenv = require("dotenv");
+const path = require("path");
+
+const verbose = core.getInput("verbose") === "true";
+
+function log(msg) {
+    if (verbose) {
+        console.log(msg);
+    }
+}
+
+const contextPath = core.getInput("path") || '.';
+
+const exit = (msg) => {
+    console.error(msg);
+    process.exit(1);
+};
+
+// Carica le variabili di ambiente dal file .env
+const envPath = path.join(contextPath, ".env");
+if (fs.existsSync(envPath)) {
+    dotenv.config({ path: envPath });
+    log("Caricate le variabili di ambiente dal file .env");
+} else {
+    exit(`File .env non trovato nel percorso ${envPath}`);
+}
+
+const packagePath = path.join(contextPath, "package.json");
+if (!fs.existsSync(packagePath)) {
+    exit(`File package.json non trovato nel percorso ${packagePath}`);
+}
+const package = require(packagePath);
+
+const buildInfoPath = path.join(contextPath, "build-info.json");
+let buildInfo = { buildNumber: 0 };
+if (fs.existsSync(buildInfoPath)) {
+    buildInfo = JSON.parse(fs.readFileSync(buildInfoPath, "utf8"));
+}
+log(`build-info.json content: ${JSON.stringify(buildInfo)}`);
+
+const port = parseInt(process.env.PORT, 10);
+
+const version = `${package.version}-${buildInfo.buildNumber}`;
+const autor = package.author.replace(" ", "_");
+
+// Valori di default
+const defaultConfig = {
+    replicas: 1,
+    targetPort: 8080,
+    namespace: "default",
+    instance: "",
+    minReplicas: 1,
+    maxReplicas: 3,
+    targetCPUUtilizationPercentage: 80
+};
+
+// Ottieni le configurazioni dagli input dell'azione, utilizzando i valori di default se non specificati
+const config = {
+    instance: core.getInput('instance') || defaultConfig.instance,
+    namespace: core.getInput('namespace') || defaultConfig.namespace,
+    serviceType: core.getInput('serviceType') || 'ClusterIP',
+    targetPort: parseInt(core.getInput('targetPort'), 10) || defaultConfig.targetPort,
+    replicas: parseInt(core.getInput('replicas'), 10) || defaultConfig.replicas,
+    registry: core.getInput('registry', { required: true }),
+    owner: core.getInput('owner', { required: true }),
+    minReplicas: parseInt(core.getInput('minReplicas'), 10) || defaultConfig.minReplicas,
+    maxReplicas: parseInt(core.getInput('maxReplicas'), 10) || defaultConfig.maxReplicas,
+    targetCPUUtilizationPercentage: parseInt(core.getInput('targetCPUUtilizationPercentage'), 10) || defaultConfig.targetCPUUtilizationPercentage
+};
+
+// Verifica che i parametri obbligatori siano presenti
+if (!config.registry) {
+    exit("Parametro 'registry' obbligatorio non fornito.");
+}
+if (!config.owner) {
+    exit("Parametro 'owner' obbligatorio non fornito.");
+}
+
+// Definisci i metadati di base utilizzati da tutti gli oggetti
+const baseMetadata = {
+    labels: {
+        "app.kubernetes.io/name": package.name,
+        "app.kubernetes.io/author": autor,
+        "app.kubernetes.io/version": version,
+        "app.kubernetes.io/instance": config.instance
+    },
+    namespace: config.namespace || "default"
+};
+
+// ConfigMap
+const configMap = {
+    apiVersion: "v1",
+    kind: "ConfigMap",
+    metadata: { ...baseMetadata, name: `${package.name}-config` },
+    data: dotenv.parse(fs.readFileSync(envPath))
+};
+
+// Service
+const service = {
+    apiVersion: "v1",
+    kind: "Service",
+    metadata: { ...baseMetadata, name: `${package.name}-service` },
+    spec: {
+        type: config.serviceType,
+        selector: baseMetadata.labels,
+        ports: [{ port: config.targetPort, targetPort: port, protocol: "TCP" }]
+    }
+};
+
+// Deployment
+const deployment = {
+    apiVersion: "apps/v1",
+    kind: "Deployment",
+    metadata: { ...baseMetadata, name: `${package.name}` },
+    spec: {
+        replicas: config.replicas,
+        selector: { matchLabels: baseMetadata.labels },
+        template: {
+            metadata: { labels: baseMetadata.labels },
+            spec: {
+                containers: [
+                    {
+                        name: package.name,
+                        image: `${config.registry}/${config.owner}/${package.name}:${version}`,
+                        ports: [{ containerPort: port }],
+                        envFrom: [{ configMapRef: { name: `${package.name}-config` } }],
+                        imagePullPolicy: "Always"
+                    }
+                ],
+                imagePullSecrets: [{ name: "regcred" }]
+            }
+        }
+    }
+};
+
+// HorizontalPodAutoscaler
+const autoscaler = {
+    apiVersion: "autoscaling/v1",
+    kind: "HorizontalPodAutoscaler",
+    metadata: { ...baseMetadata, name: `${package.name}-autoscaler` },
+    spec: {
+        scaleTargetRef: {
+            apiVersion: "apps/v1",
+            kind: "Deployment",
+            name: package.name
+        },
+        minReplicas: config.minReplicas,
+        maxReplicas: config.maxReplicas,
+        targetCPUUtilizationPercentage: config.targetCPUUtilizationPercentage
+    }
+};
+
+// Opzioni YAML
+const yamlOptions = {
+    noRefs: true,
+    styles: { "!!str": "double" }
+};
+
+const yamlContents = [];
+yamlContents.push(yaml.dump(configMap, yamlOptions));
+yamlContents.push(yaml.dump(service, yamlOptions));
+yamlContents.push(yaml.dump(deployment, yamlOptions));
+yamlContents.push(yaml.dump(autoscaler, yamlOptions));
+
+const allYaml = yamlContents.join("\n---\n");
+
+const directoryPath = path.join(__dirname, "k8s"); // Imposta il percorso della directory
+
+// Verifica se la directory esiste
+if (!fs.existsSync(directoryPath)) {
+    // Se non esiste, creala
+    fs.mkdirSync(directoryPath, { recursive: true });
+    log(`Creata la directory ${directoryPath}`);
+}
+
+// Scrive il file YAML finale
+fs.writeFile(path.join(directoryPath, "all-in-one.yaml"), allYaml, "utf8", (err) => {
+    if (err) {
+        console.error("Errore durante la scrittura del file YAML:", err);
+        core.setFailed(`Errore durante la scrittura del file YAML: ${err.message}`);
+    } else {
+        log("File YAML all-in-one creato con successo!");
+    }
+});
